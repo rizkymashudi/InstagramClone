231ee2d3fb66071a688ae567abcd6df6df8ece03
rimas12 <rizky_mashudy@yahoo.co.id>
2021-09-04 20:06:04 +0700

    first commit

--- InstagramClone/Controllers/Core Tabs/ExploreViewController.swift
+++ InstagramClone/Controllers/Core Tabs/ExploreViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ExploreViewController.swift
 //  InstagramClone
 //
 //  Created by Rizky Mashudi on 04/09/21.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ExploreViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
+    
 
 }
-
---
+++ InstagramClone/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Rizky Mashudi on 04/09/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
